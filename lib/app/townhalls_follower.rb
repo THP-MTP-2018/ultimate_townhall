fb0f834c8441f2a66f355acde8b8adf855543183
Anouarsoufyani <anouar.soufyani@gmail.com>
2018-07-19 07:52:19 -0400

    Anouar a commité le fichier follower

---
+++ lib/app/townhalls_follower.rb
@@ -0,0 +1,22 @@
+require "dotenv"
+require "pry"
+require "twitter"
+
+Dotenv.load
+
+class follower
+  def inizialize
+    
+  end
+end
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["TWITTER_API_KEY"]
+  config.consumer_secret     = ENV["TWITTER_API_KEY_SECRET"]
+  config.access_token        = ENV["TWITTER_API_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_API_TOKEN_SECRET"]
+end
+
+search.each do |handle|
+  client.follow(handle)
+end
