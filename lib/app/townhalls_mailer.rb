ceb6e81070a4f55dc3720e9fc2ee27227f885b6e
jibenight <40353151+jibenight@users.noreply.github.com>
2018-07-20 06:02:16 +0200

    insert charles names

--- lib/app/townhalls_mailer.rb
+++ lib/app/townhalls_mailer.rb
@@ -30,7 +30,7 @@ class Mailer
       subject "École The Hacking Project "
       body "Bonjour,
 
-    Je m'appelle Charlito, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
+    Je m'appelle Charles, je suis élève à The Hacking Project, une formation au code gratuite, sans locaux, sans sélection, sans restriction géographique. La pédagogie de ntore école est celle du peer-learning, où nous travaillons par petits groupes sur des projets concrets qui font apprendre le code. Le projet du jour est d'envoyer (avec du codage) des emails aux mairies pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation pour tous.
 
     Déjà 500 personnes sont passées par The Hacking Project. Est-ce que la mairie de #{name} veut changer le monde avec nous ?
 
